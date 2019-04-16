728052e108f2db294be7507dcf2d4fc2abff0f8c
adrianolego <adriano__silva__@hotmail.com>
2019-04-16 16:06:14 -0300

    COnfigurações config server

--- configServer/src/main/java/com/adriano/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/com/adriano/configServer/ConfigServerApplication.java
@@ -3,10 +3,9 @@ package com.adriano.configServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableConfigServer
-@EnableEurekaServer
+//@EnableEurekaServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
--- controledecoleta/src/main/java/com/adriano/controledecoleta/ControledecoletaApplication.java
+++ controledecoleta/src/main/java/com/adriano/controledecoleta/ControledecoletaApplication.java
@@ -5,14 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class ControledecoletaApplication {
 
