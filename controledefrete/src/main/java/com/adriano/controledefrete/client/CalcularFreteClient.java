05bfdaf08cba00ea4ac71087df64162a8ad2678e
adrianolego <adriano__silva__@hotmail.com>
2019-05-03 20:09:11 -0300

    Configs feign

--- controledefrete/src/main/java/com/adriano/controledefrete/client/CalcularFreteClient.java
+++ controledefrete/src/main/java/com/adriano/controledefrete/client/CalcularFreteClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("integrador.frete")
+@FeignClient("integrador-frete")
 public interface CalcularFreteClient {
 
     @RequestMapping("/freteExterno")
--- controledefrete/src/main/java/com/adriano/controledefrete/client/RegistrarFaturamentoClient.java
+++ controledefrete/src/main/java/com/adriano/controledefrete/client/RegistrarFaturamentoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("integrador.faturamento")
+@FeignClient("integrador-faturamento")
 public interface RegistrarFaturamentoClient {
 
     @PostMapping("/registrarFaturamento")
