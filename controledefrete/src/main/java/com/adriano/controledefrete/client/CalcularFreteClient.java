8995d139802794a07cc11be8c7ac06903c1d5b56
adrianolego <adriano__silva__@hotmail.com>
2019-05-06 08:49:14 -0300

    alteração frete discovery

--- controledefrete/src/main/java/com/adriano/controledefrete/client/CalcularFreteClient.java
+++ controledefrete/src/main/java/com/adriano/controledefrete/client/CalcularFreteClient.java
@@ -2,16 +2,12 @@ package com.adriano.controledefrete.client;
 
 import com.adriano.controledefrete.client.dto.EntradaFreteDTO;
 import com.adriano.controledefrete.client.dto.RetornoFreteDTO;
-import com.adriano.controledefrete.model.Frete;
-import com.adriano.controledefrete.model.PedidoEncomenda;
-import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("integrador-frete")
+@FeignClient(name = "integrador", path = "/frete", contextId = "frete")
 public interface CalcularFreteClient {
 
     @RequestMapping("/freteExterno")
--- controledefrete/src/main/java/com/adriano/controledefrete/client/RegistrarFaturamentoClient.java
+++ controledefrete/src/main/java/com/adriano/controledefrete/client/RegistrarFaturamentoClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("integrador-faturamento")
+@FeignClient(name = "integrador", path = "/faturamento", contextId = "faturamento")
+
 public interface RegistrarFaturamentoClient {
 
     @PostMapping("/registrarFaturamento")
