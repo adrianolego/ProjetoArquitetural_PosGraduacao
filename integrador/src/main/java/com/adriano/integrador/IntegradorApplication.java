d04d1b07cd03abab3491f33b69ae38d00d5de734
adrianolego <adriano__silva__@hotmail.com>
2019-05-09 17:28:22 -0300

    Eureka configs

--- integrador/src/main/java/com/adriano/integrador/IntegradorApplication.java
+++ integrador/src/main/java/com/adriano/integrador/IntegradorApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
+
 @SpringBootApplication
 public class IntegradorApplication {
 
