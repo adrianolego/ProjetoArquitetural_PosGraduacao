b85f2c007b7cabc9db631a42ebdaeca1f6db658c
adrianolego <adriano__silva__@hotmail.com>
2019-05-09 14:04:06 -0300

    Eureka configs

--- controledecoleta/src/main/java/com/adriano/controledecoleta/ControledecoletaApplication.java
+++ controledecoleta/src/main/java/com/adriano/controledecoleta/ControledecoletaApplication.java
@@ -2,10 +2,8 @@ package com.adriano.controledecoleta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
 public class ControledecoletaApplication {
