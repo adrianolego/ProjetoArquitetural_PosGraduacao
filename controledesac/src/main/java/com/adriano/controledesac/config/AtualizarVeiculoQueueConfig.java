e253de4083fff6c664d780b8f07f63152edb365d
adrianolego <adriano__silva__@hotmail.com>
2019-05-10 07:56:05 -0300

    Configs Eureka

--- controledesac/src/main/java/com/adriano/controledesac/config/AtualizarVeiculoQueueConfig.java
+++ controledesac/src/main/java/com/adriano/controledesac/config/AtualizarVeiculoQueueConfig.java
@@ -1,6 +1,10 @@
 package com.adriano.controledesac.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
