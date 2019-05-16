55d320d99955e4c5811bfe50cd46c672c9dfb3b1
adrianolego <adriano__silva__@hotmail.com>
2019-05-16 13:44:30 -0300

    Correção do yml

--- controledesac/src/main/java/com/adriano/controledesac/service/CacheService.java
+++ controledesac/src/main/java/com/adriano/controledesac/service/CacheService.java
@@ -3,20 +3,18 @@ package com.adriano.controledesac.service;
 import com.adriano.controledesac.document.Encomenda;
 import com.adriano.controledesac.repository.EncomendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CacheService {
+
     @Autowired
     private EncomendaRepository encomendaRepository;
 
-    @Value("${config.redis.enabled}")
-
-    @Cacheable(value = "encomenda", key = "#idEncomenda")
-    @CachePut
+//    @Value("${config.redis.enabled}")
+//
+//    @Cacheable(value = "encomenda", key = "#idEncomenda")
+//    @CachePut
     public Encomenda inserirCache(String idEncomenda) {
         return encomendaRepository.findByIdEncomenda(idEncomenda);
 
