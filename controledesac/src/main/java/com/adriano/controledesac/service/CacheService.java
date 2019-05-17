f11a0c0e07ac58e338e12e07314278a24d97fb17
adrianolego <adriano__silva__@hotmail.com>
2019-05-17 08:35:37 -0300

    redis cache

--- controledesac/src/main/java/com/adriano/controledesac/service/CacheService.java
+++ controledesac/src/main/java/com/adriano/controledesac/service/CacheService.java
@@ -3,6 +3,8 @@ package com.adriano.controledesac.service;
 import com.adriano.controledesac.document.Encomenda;
 import com.adriano.controledesac.repository.EncomendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,18 +13,19 @@ public class CacheService {
     @Autowired
     private EncomendaRepository encomendaRepository;
 
-//    @Value("${config.redis.enabled}")
-//
-//    @Cacheable(value = "encomenda", key = "#idEncomenda")
-//    @CachePut
-    public Encomenda inserirCache(String idEncomenda) {
+    @Value("${config.redis.enabled}")
+    private Boolean cacheEnabled;
+
+    @CachePut(value = "encomenda",
+            key = "#idEncomenda",
+            condition = "#root.target.getCacheEnabled()",
+            unless = "#result != null")
+    public Encomenda atualizarCache(String idEncomenda) {
         return encomendaRepository.findByIdEncomenda(idEncomenda);
 
     }
 
-//    @Cacheable(value = "encomenda", key = "#idEncomenda")
-//    @CachePut
-//    public Encomenda atualizarCache(String idEncomenda) {
-//        return encomendaRepository.findByIdEncomenda(idEncomenda);
-//    }
+    public Boolean getCacheEnabled() {
+        return cacheEnabled;
+    }
 }
--- controledesac/src/main/java/com/adriano/controledesac/service/EncomendaService.java
+++ controledesac/src/main/java/com/adriano/controledesac/service/EncomendaService.java
@@ -5,6 +5,8 @@ import com.adriano.controledesac.model.PedidoEncomenda;
 import com.adriano.controledesac.repository.EncomendaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
@@ -19,16 +21,29 @@ public class EncomendaService {
     private ConversionService conversionService;
 
     @Autowired
-    CacheService cacheService;
+    private CacheService cacheService;
+
+    @Value("${config.redis.enabled}")
+    private Boolean cacheEnabled;
+
 
     public void salvarPedido(PedidoEncomenda encomenda) {
         Encomenda e = conversionService.convert(encomenda, Encomenda.class);
         encomendaRepository.save(e);
 
-        cacheService.inserirCache(e.getIdEncomenda());
+        cacheService.atualizarCache(e.getIdEncomenda());
     }
 
+    @Cacheable(value = "encomenda",
+            key = "#idEncomenda",
+            condition = "#root.target.getCacheEnabled()",
+            unless = "#result != null")
     public Encomenda consultarEncomenda(String idEncomenda) {
         return encomendaRepository.findByIdEncomenda(idEncomenda);
+    }
+
+
+    public Boolean getCacheEnabled() {
+        return cacheEnabled;
     }
 }
